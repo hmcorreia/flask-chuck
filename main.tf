43e640703c060852a70258636a192ee929547cc3
hmcorreia <hmcorreia@gmail.com>
2024-11-13 00:29:28 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
-# main.tf
 terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
-      version = "3.0.2"
+      version = "2.13.0"
     }
   }
+  required_version = ">= 0.13"
 }
 
-provider "docker" {
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
 }
 
-resource "docker" "hmcorreia1977" {
-	name = "flask-chuck:version1"
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
